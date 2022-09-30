f7a6d0819abfc9077d673f2de6d1532b83dc227e
abhm1999 <abdullahmohdi1999@gmail.com>
2022-09-30 23:32:57 +0530

    day3

---
+++ src/Pages/PaymentSucc.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function PaymentSucc() {
+  return (
+    <div>PaymentSucc</div>
+  )
+}
--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,19 +1,19 @@
-// import { legacy_createStore, compose } from "redux";
-// import { combineReducers } from "redux";
-// import { applyMiddleware } from "redux";
-// import thunk from "redux-thunk";
-// import { reducer } from "./App/reducer";
-// const rootReducer = combineReducers({
-//     reducer
-// });
+import { legacy_createStore, compose } from "redux";
+import { combineReducers } from "redux";
+import { applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { reducer } from "./App/reducer";
+const rootReducer = combineReducers({
+    reducer
+});
 
-// const devtools = () => {
-//   return (
-//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-//   );
-// };
+const devtools = () => {
+  return (
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
+};
 
-// export const store = legacy_createStore(
-//   rootReducer,
-//   compose(applyMiddleware(thunk), devtools())
-// );
+export const store = legacy_createStore(
+  rootReducer,
+  compose(applyMiddleware(thunk), devtools())
+);
